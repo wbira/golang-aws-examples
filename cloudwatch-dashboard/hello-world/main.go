150355c42d4c720b07431cc6377fccc7c884f519
Waldemar Bira <biraw@ryanair.com>
2020-03-25 21:07:35 +0100

    Created dashboard and added errors

--- cloudwatch-dashboard/hello-world/main.go
+++ cloudwatch-dashboard/hello-world/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net/http"
 
@@ -8,10 +9,15 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// handlers returns errors to generate logs on dashboard
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	log.Printf("Info: recived request %v", request)
 
-	if _, ok := request.QueryStringParameters["error"]; ok {
+	if value, ok := request.QueryStringParameters["error"]; ok {
+		if value == "notfound" {
+			log.Print("Error: not found")
+			return createApiGatewayResponse("Not found\n", http.StatusNotFound)
+		}
 		log.Print("Error: internal server error")
 		return createApiGatewayResponse("Error\n", http.StatusInternalServerError)
 	}
@@ -20,10 +26,15 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 }
 
 func createApiGatewayResponse(body string, statusCode int) (events.APIGatewayProxyResponse, error) {
+	var err error
+
+	if statusCode == http.StatusInternalServerError {
+		err = errors.New("internal server error")
+	}
 	return events.APIGatewayProxyResponse{
 		Body:       body,
 		StatusCode: statusCode,
-	}, nil
+	}, err
 }
 
 func main() {
