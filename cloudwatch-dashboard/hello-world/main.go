0944c7c88a5f25c76e3d7594d4088a061c3e81e7
Waldemar Bira <biraw@ryanair.com>
2020-03-25 12:02:41 +0100

    Created simple lambda with logger

--- cloudwatch-dashboard/hello-world/main.go
+++ cloudwatch-dashboard/hello-world/main.go
@@ -1,48 +1,28 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var (
-	// DefaultHTTPGetAddress Default Address
-	DefaultHTTPGetAddress = "https://checkip.amazonaws.com"
-
-	// ErrNoIP No IP found in response
-	ErrNoIP = errors.New("No IP in HTTP response")
-
-	// ErrNon200Response non 200 status code in response
-	ErrNon200Response = errors.New("Non 200 Response found")
-)
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	resp, err := http.Get(DefaultHTTPGetAddress)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
+	log.Printf("Info: recived request %v", request)
+
+	if _, ok := request.QueryStringParameters["error"]; ok {
+		log.Print("Error: internal server error")
+		return createApiGatewayResponse("Error\n", http.StatusInternalServerError)
 	}
 
-	if resp.StatusCode != 200 {
-		return events.APIGatewayProxyResponse{}, ErrNon200Response
-	}
+	return createApiGatewayResponse("Success\n", http.StatusOK)
+}
 
-	ip, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	if len(ip) == 0 {
-		return events.APIGatewayProxyResponse{}, ErrNoIP
-	}
-
+func createApiGatewayResponse(body string, statusCode int) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
-		Body:       fmt.Sprintf("Hello, %v", string(ip)),
-		StatusCode: 200,
+		Body:       body,
+		StatusCode: statusCode,
 	}, nil
 }
 
